d3f351fc226303d78827ec77efe7e15bd815404a
Benjamin Vic <benjamin.vic@etu.univ-paris-diderot.fr>
2016-11-22 22:05:36 +0100

    Fist commit

---
+++ code/src/mmu.c
@@ -0,0 +1,23 @@
+#ifndef __MMU__C__
+#define __MMU__C__
+
+#include "../header/mmu.h"
+
+int main() {
+	mem_t *mem = initMem();
+	
+	address_t adr1 = myAlloc(mem, 5);
+	address_t adr2 = myAlloc(mem, 10);
+	
+	address_t adr3 = myAlloc(mem, 100);
+	myFree(mem, adr2, 10);
+	myFree(mem, adr1, 5);
+	
+	myWrite(mem, adr3, 543); // write on the 1 st byte
+	myWrite(mem, adr3+9, 34); // write on the 10th byte
+	
+	byte_t val1 = myRead(mem, adr3);
+	byte_t val2 = myRead(mem, adr3+9);
+}
+
+#endif
