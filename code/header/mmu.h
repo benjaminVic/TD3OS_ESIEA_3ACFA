57461abf12263a208a07914fb6c9f38d86fefaed
Benjamin Vic <benjamin.vic@etu.univ-paris-diderot.fr>
2016-11-27 22:50:04 +0100

    More Things done

--- code/header/mmu.h
+++ code/header/mmu.h
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef int address_t;
 
@@ -17,17 +18,22 @@ typedef struct hole {
 
 typedef struct mem {
 	bool mem[SIZE];
-	hole_t root; // holes list
+	hole_t* root; // holes list
 } mem_t;
 
 // dynamically allocates a mem_t structure and initializes its content
 mem_t* initMem();
 
-// allocates space in bytes (bool) using First-Fit, Best-Fit or Worst-Fit
-/*address_t myAlloc(mem_t* mp, int sz);
+// allocates space in bytes (bool) using First-Fit
+address_t myAlloc(mem_t* mp, int sz);
+
+// allocates space in bytes (bool) using Best-Fit
+
+// allocates space in bytes (bool) using Worst-Fit
+
 
 // release memory that has already been allocated previously
-void myFree(mem_t* mp, address_t p, int sz);
+/*void myFree(mem_t* mp, address_t p, int sz);
 
 // assign a value to a byte
 void myWrite(mem_t* mp, address_t p, bool val);
--- code/src/mmu.c
+++ code/src/mmu.c
@@ -6,10 +6,10 @@
 int main() {
 	mem_t *mem = initMem();
 	
-	/*address_t adr1 = myAlloc(mem, 5);
+	address_t adr1 = myAlloc(mem, 5);
 	address_t adr2 = myAlloc(mem, 10);
 	
-	address_t adr3 = myAlloc(mem, 100);
+	/*address_t adr3 = myAlloc(mem, 100);
 	myFree(mem, adr2, 10);
 	myFree(mem, adr1, 5);
 	
@@ -32,26 +32,47 @@ mem_t* initMem(){
 
 	//On init le premier hole_t
 	//TODO
+	mp->root = NULL;
 
 	return mp;
-	
-	address_t adr = myRead(mem_t* mem, address_t p);
-	int sz = malloc(sizeof(bool));
-	struct hole* next = 1;
-	struct hole* prev = NULL;
-	
-	do{
-	address_t adr = myRead(mem_t* mp, address_t p);
-	int sz = malloc(sizeof(bool));
-	struct hole* next = SIZE+1;
-	struct hole* prev = SIZE-1;
-	}while(SIZE<65535)
-	
-	address_t adr = myRead(mem_t* mp, address_t p);
-	int sz = malloc(sizeof(bool));
-	struct hole* next = NULL;
-	struct hole* prev = SIZE;
-	
+}
+
+address_t myAlloc(mem_t* mp, int sz){
+	address_t adresseDebutAllocation = -1;
+
+	if (mp->root == NULL){
+
+		//Aucun noeud n'est affecté donc on affecte dès le début de la liste
+		hole_t* hole = malloc(sizeof(hole_t));
+		hole->adr = 0;
+		hole->sz = sz;
+		hole->next = NULL;
+		hole->prev = NULL;
+		mp->root = hole;
+		//TODO ADD LOOP AND WRITE WITHIN mp
+		adresseDebutAllocation = 0;
+
+	} else { //Pas la première affectation
+
+		hole_t* hole = mp->root;
+		int firstAdr = 0;
+		//TODO On recherche la première adresse libre
+		while (hole->next != NULL) {
+			firstAdr = firstAdr + hole->sz;
+			hole = hole->next;
+		}
+
+		//TODO ADD LOOP AND WRITE WITHIN mp
+		hole_t* newHole = malloc(sizeof(hole_t));
+		newHole->adr = firstAdr;
+		newHole->sz = sz;
+		newHole->next = NULL;
+		newHole->prev = hole;
+		hole->next = newHole;
+
+	}
+
+	return adresseDebutAllocation;
 }
 
 #endif
